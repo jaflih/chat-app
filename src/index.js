de09223dff13a67cd1e21e72231a61421c7eba01
jihaneh <jijih@hotmail.fr>
2022-10-25 18:02:47 +0200

    Add Routes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Connexion from './components/Connexion';
+import NotFound from './components/NotFound';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Root = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Connexion />} />
+      <Route path="/pseudo/:pseudo" element={<App />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Connexion />
+    <Root />
   </React.StrictMode>
 );
 
