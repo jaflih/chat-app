9f38d9a3c3b6745841ca899fe8e629c91bacebf3
jihaneh <jijih@hotmail.fr>
2022-10-25 19:21:02 +0200

    Add formulaire and message

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import Message from './components/Message';
 import './App.css';
 
 class App extends Component {
+  state = {
+    messages: {},
+    pseudo: 'jihane', //this.props.match.params.pseudo,
+  };
+
+  addMessage = (message) => {
+    const messages = { ...this.state.messages };
+    messages[`message-${Date.now()}`] = message;
+    this.setState({ messages });
+  };
+
   render() {
+    const messages = Object.keys(this.state.messages).map((key) => (
+      <Message key={key} pseudo={this.state.messages[key].pseudo} message={this.state.messages[key].message} />
+    ));
+
+    console.log(messages);
     return (
       <div className="box">
         <div>
-          <div className="messages">
-            <Message />
-          </div>
+          <div className="messages">{messages}</div>
         </div>
-        <Formulaire />
+        <Formulaire addMessage={this.addMessage} pseudo={this.state.pseudo} length={140} />
       </div>
     );
   }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Message = () => {
-  return <p className="user-message">Mon Message</p>;
+const Message = ({ pseudo, message }) => {
+  return <p className="user-message">{message}</p>;
 };
 
 export default Message;
