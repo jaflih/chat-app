d9156f10c9c5df87c5d92090538a28da33ed0081
jihaneh <jijih@hotmail.fr>
2022-10-25 21:47:40 +0200

    Add transition

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component, createRef } from 'react';
 import Formulaire from './components/Formulaire';
 import Message from './components/Message';
 import './App.css';
+import './animations.css';
 
 import { db } from './firebase';
 import { ref, set, get, update, remove, child } from 'firebase/database';
+
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class App extends Component {
   state = {
@@ -25,29 +28,41 @@ class App extends Component {
   }
 
   addMessage = (message) => {
-    const messages = { ...this.state.messages };
+    let messages = { ...this.state.messages };
     const messageId = `message-${Date.now()}`;
 
     messages[messageId] = message;
-    this.setState({ messages });
-    const db = this.state.db;
 
+    if (Object.keys(messages).length > 10) {
+      messages = Object.keys(messages).slice(0, 1);
+    }
+    /* Object.keys(messages)
+      .slice(0, -10)
+      .forEach(key => {
+        messages[key] = null;
+      });
+*/
+    this.setState({ messages });
+    console.log(messages);
+
+    /*const db = this.state.db;
     set(ref(db, 'messages/' + messageId), {
       messages: message,
-    });
+    });*/
   };
 
   render() {
     const messages = Object.keys(this.state.messages).map((key) => (
-      <Message key={key} pseudo={this.state.messages[key].pseudo} message={this.state.messages[key].message} />
+      <CSSTransition timeout={2000} classNames="fade" key={key}>
+        <Message pseudo={this.state.messages[key].pseudo} message={this.state.messages[key].message} />
+      </CSSTransition>
     ));
 
-    console.log(messages);
     return (
       <div className="box">
         <div>
           <div className="messages" ref={this.messagesRef}>
-            {messages}
+            <TransitionGroup className="message">{messages}</TransitionGroup>
           </div>
         </div>
         <Formulaire addMessage={this.addMessage} pseudo={this.state.pseudo} length={140} />
