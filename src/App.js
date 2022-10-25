2b9f220687d16f99807001d450ec27226302f3f8
jihaneh <jijih@hotmail.fr>
2022-10-25 18:24:30 +0200

    Add Formulaire and Message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Formulaire from './components/Formulaire';
+import Message from './components/Message';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="box">
+        <div>
+          <div className="messages">
+            <Message />
+          </div>
+        </div>
+        <Formulaire />
+      </div>
+    );
+  }
 }
 
 export default App;
