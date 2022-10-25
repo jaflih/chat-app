c7cf48d15a0fcde62dc258ab512cdd39a269ec7a
jihaneh <jijih@hotmail.fr>
2022-10-25 21:14:57 +0200

    Add firebase And ref

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import Formulaire from './components/Formulaire';
 import Message from './components/Message';
 import './App.css';
 
+import { db } from './firebase';
+import { ref, set, get, update, remove, child } from 'firebase/database';
+
 class App extends Component {
   state = {
+    db: '',
     messages: {},
     pseudo: 'jihane', //this.props.match.params.pseudo,
   };
 
+  messagesRef = createRef();
+
+  componentDidMount() {
+    this.setState({ db: db });
+  }
+
+  componentDidUpdate() {
+    const ref = this.messagesRef.current;
+    ref.scrollTop = ref.scrollHeight;
+  }
+
   addMessage = (message) => {
     const messages = { ...this.state.messages };
-    messages[`message-${Date.now()}`] = message;
+    const messageId = `message-${Date.now()}`;
+
+    messages[messageId] = message;
     this.setState({ messages });
+    const db = this.state.db;
+
+    set(ref(db, 'messages/' + messageId), {
+      messages: message,
+    });
   };
 
   render() {
@@ -24,7 +46,9 @@ class App extends Component {
     return (
       <div className="box">
         <div>
-          <div className="messages">{messages}</div>
+          <div className="messages" ref={this.messagesRef}>
+            {messages}
+          </div>
         </div>
         <Formulaire addMessage={this.addMessage} pseudo={this.state.pseudo} length={140} />
       </div>
